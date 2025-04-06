1564486acf0b897878d0ec4c8590d970cf31bca5
marioillan <marioillan12@gmail.com>
2025-04-06 15:16:51 +0200

    app.jsx + app.css + index.css

--- supercomparador/src/App.jsx
+++ supercomparador/src/App.jsx
@@ -1,35 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/logo.png';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <a href="#"><img src={logo} alt="logo supercomparador"/></a>
+
+        <nav>
+          <ul>
+            <li><a href="#">Supermercados</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Sobre Nosotros</a></li>
+          </ul>
+        </nav>
+
+        <div className='usuario-inicio'>
+          <div className='inicio-sesion'>
+          <input 
+              type="text" 
+              placeholder="Nombre Usuario" 
+              name="usuario"
+          />
+          <input 
+              type="password" 
+              placeholder="Contraseña" 
+              name="contraseña"
+          />
+            <button type="submit" className='boton-usuario'>Iniciar Sesión</button>
+          </div>
+          <button className='boton-usuario'><a href="#">Registrarse</a></button>
+        </div>
+
+      </header>
+
+      <main>
+        <section>
+          <h2>Buscar productos:</h2>
+          <form>
+            <input 
+              type="text" 
+              placeholder="Buscar..." 
+              name="busqueda"
+            />
+            <button type="submit" className='boton-buscar'>Buscar</button>
+          </form>
+        </section>
+
+      </main>
+
+      <footer>
+
+
+
+      </footer>
+
     </>
-  )
+
+  );
 }
 
 export default App
